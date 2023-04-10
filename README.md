08756c21b512f8514b90454279e9a5d7f371bf64
LocheMan <96348438+LocheMan@users.noreply.github.com>
2023-04-10 14:04:11 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Programme Python : Readme
+
+Ce programme en Python est conçu pour supprimer les traces d'utilisation de certaines applications de Windows (une config a été fait pour 2 cheats Minecraft). Il est important de noter que l'utilisation de ce programme peut causer des dommages à votre système et est donc à vos propres risques. Le programme lit la configuration depuis le fichier `config.json`.
+
+## Utilisation du programme
+
+### Configuration
+
+Avant de lancer le programme, vous devez configurer les paramètres dans le fichier `config.json`. Les paramètres disponibles sont :
+
+- `"cheats"` : la liste des fichiers, répertoires et logs à supprimer.
+
+Vous devez remplir les champs correspondants dans le fichier `config.json` avant d'exécuter le programme.
+
+### Lancement du programme
+
+Une fois que vous avez configuré les paramètres dans le fichier `config.json`, vous pouvez lancer le programme en exécutant le fichier `main.py`.
+
+Le programme va supprimer les fichiers, répertoires et logs spécifiés dans la configuration.
+
+### Remarque
+
+- L'utilisation de ce script pour supprimer des fichiers, répertoires ou logs qui sont nécessaires pour le fonctionnement de votre système peut causer des dommages irréparables à votre système.
+- L'auteur de ce script n'est pas responsable des dommages pouvant résulter de l'utilisation de ce script.
+
+## Licence
+
+Ce programme est sous licence MIT. Veuillez consulter le fichier `LICENSE` pour plus d'informations.
---
+++ main.py
@@ -0,0 +1,107 @@
+import os
+import json
+
+try: import pystyle
+except:
+    try: os.system(f"pip uninstall pystyle")
+    except: pass
+    os.system(f"pip install pystyle")
+    
+try: import shutil
+except:
+    try: os.system(f"pip uninstall shutil")
+    except: pass
+    os.system(f"pip install shutil")
+
+
+def remover():
+    data = json.load(open('config.json', 'r'))
+    user = os.getenv("UserName")
+
+    # Prefetch
+    prefetches = data["cheats"]["prefetches"]
+    try:
+        for file in os.listdir("C:/Windows/prefetch"):
+            for i in range(len(prefetches)):
+                if prefetches[i] in file:
+                    os.remove(f"C:/Windows/prefetch/{file}")
+                    print(f"Le prefetch du programme \"{prefetches[i]}\" a été supprimé")
+    except:
+        print("Erreur: Prefetches")
+
+    # RoamingState Directory
+    directories = data["cheats"]["directories"]
+    try:
+        for i in range(len(directories)):
+            try:
+                shutil.rmtree(f"C:/Users/{user}/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/RoamingState/{directories[i]}")
+                print(f"Le répertoire \"{directories[i]}\" a été supprimé")
+            except:
+                pass
+    except:
+        print("Erreur: Directories")
+
+    # Temp
+    temp = data["cheats"]["temp"]
+    try:
+        for file in os.listdir(f"C:/Users/{user}/AppData/Local/Temp/"):
+            for i in range(len(temp)):
+                if temp[i] in file:
+                    os.remove(f"C:/Users/{user}/AppData/Local/Temp/{file}")
+                    print(f"Le fichier temporaire \"{temp[i]}\" a été supprimé")
+    except:
+        print("Erreur: Temp\n\n")
+
+    # Local
+    local = data["cheats"]["local"]
+    try:
+        for i in range(len(local)):
+            try:
+                shutil.rmtree(f"C:/Users/{user}/AppData/Local/{local[i]}")
+                print(f"Le répertoire local \"{local[i]}\" a été supprimé")
+            except:
+                pass
+    except:
+        print("Erreur: Local")
+    
+    # UsageLogs
+    usageLogs = data["cheats"]["usageLogs"]
+    try:
+        for file in os.listdir(f"C:/Users/{user}/AppData/Local/Microsoft/CLR_v4.0/UsageLogs"):
+            for i in range(len(usageLogs)):
+                if usageLogs[i] in file:
+                    os.remove(f"C:/Users/{user}/AppData/Local/Microsoft/CLR_v4.0/UsageLogs/{file}")
+                    print(f"Le log \"{usageLogs[i]}\" a été supprimé")
+    except Exception as e:
+        print("Erreur: usageLogs\n\n")
+    
+    # Logs
+    logs = data["cheats"]["logs"]
+    try:
+        for file in os.listdir(f"C:/Users/{user}/AppData/Local/Microsoft/CLR_v4.0/UsageLogs"):
+            for i in range(len(logs)):
+                if logs[i] in file:
+                    os.remove(f"C:/Users/{user}/AppData/Local/Microsoft/CLR_v4.0/UsageLogs/{file}")
+                    print(f"Le log \"{logs[i]}\" a été supprimé")
+    except Exception as e:
+        print("Erreur: Logs\n\n")
+
+    # Dossier remover
+    try:
+        shutil.rmtree(f"remover")
+    except Exception as e:
+        print("Erreur: Remover")
+    
+    
+    os.makedirs('remover', exist_ok=True)
+    try: open("remover/REMOVER", "x")
+    except: pass
+
+
+    # Stop
+    print(f"\n {pystyle.Colors.cyan}Les traces de cheat sont maintenant supprimés{pystyle.Colors.red}")
+
+remover()
+
+os.system('timeout 5')
+print(pystyle.Colors.reset)
